36bc1b54edf3579112a427cc94ab590e1bf6d736
rohit-verma-au7 <58546247+rohit-verma-au7@users.noreply.github.com>
2020-09-04 01:05:08 +0530

    rishu

---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@2020 Rishu Raj</span>
+      </div>
+    </footer>
+  );
+};
